11f6d72ef7d1dd2fcf249266855fb3b624fee0de
yoshiki20010524 <75286616+yoshiki20010524@users.noreply.github.com>
2022-03-02 17:58:51 +0900

    add-file

---
+++ love-consultation/love_consultationApp.swift
@@ -0,0 +1,17 @@
+//
+//  love_consultationApp.swift
+//  love-consultation
+//
+//  Created by yoshikinaruo on 2022/03/02.
+//
+
+import SwiftUI
+
+@main
+struct love_consultationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
